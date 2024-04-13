64788879b5717c16b097a43957f022ae95094ed2
wanglei <806416060@qq.com>
2024-04-13 17:47:16 +0800

    unplugin-vue-router unocss等

---
+++ src/pages/about.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div>
+about
+  </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import HelloWorld from '../components/HelloWorld.vue'
+
+</script>
+
+<template>
+  <div>
+    <HelloWorld  msg=""/>
+  </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
